eae8b21ad46716bd57009e3e0fd7c385572de747
Praveen Perera <praveen@avencera.com>
2021-09-29 10:29:01 -0400

    Update yamine.rb

--- pkg/brew/yamine.rb
+++ pkg/brew/yamine.rb
@@ -7,7 +7,7 @@
 #   brew untap avencera/yamine
 
 class Yamine < Formula
-  version 'v0.1.0'
+  version 'v0.1.1'
   desc "A simple CLI for combining json and yaml files"
   homepage "https://github.com/avencera/yamine"
   license "Apache-2.0"
@@ -15,22 +15,22 @@ class Yamine < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/avencera/yamine/releases/download/#{version}/yamine-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "9789325e16bfb4443e64f1e7fc036f6f5e74bf853aab0e7898b7b36cef0e69a5"
+    sha256 "38981ed024379f9060278d1859dd482867345b7067200697fe49f41afcb89e29"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/avencera/yamine/releases/download/#{version}/yamine-#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "9c4c8a7adf8fcf99c4d955d5803fd383719f83458228d0866672608948b40b55"
+    sha256 "69713402fb4a5e33ac9d7460613e4c4f9ceeac9ed30c236912cf1307a49601e2"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/avencera/yamine/releases/download/#{version}/yamine-#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "212144e6025de75651b3942feadecc387d8f0302bf4d6bc4947d395d13112c32"
+    sha256 "5b5803dbf35a22f5233eb0e4c13c6903b80e24eb09fb1b84900241440b48eed9"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/avencera/yamine/releases/download/#{version}/yamine-#{version}-aarch64-unknown-linux-musl.tar.gz"
-    sha256 "4847d632fb01ad8619a827fa11fb0e2b45d3c66e7ab0ce8dd2c0037b4e1513bc"
+    sha256 "0de17cbaa2c302ec4917c6bbf56f9a0290b9a234b0ef3742d39d16dc870cf965"
   end
 
   def install
