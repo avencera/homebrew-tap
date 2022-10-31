36c0333627948b7423d600ff28110a39157f3676
Praveen Perera <praveen@avencera.com>
2022-10-31 09:53:09 -0500

    Add brew forumla for RustyWind

--- pkg/brew/rustywind.rb
+++ pkg/brew/rustywind.rb
@@ -1,36 +1,36 @@
 # To install:
 #   brew tap avencera/taps
-#   brew install yamine
+#   brew install rustywind
 #
 # To remove:
-#   brew uninstall yamine
+#   brew uninstall rustywind
 #   brew untap avencera/taps
-class Yamine < Formula
+class Rustywind < Formula
   name 'rustywind'
   version 'v0.2.0'
-  desc "A simple CLI for combining json and yaml files"
-  homepage "https://github.com/avencera/yamine"
+  desc "CLI for organizing Tailwind CSS classes"
+  homepage "https://github.com/avencera/rustywind"
   license "Apache-2.0"
 
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/avencera/yamine/releases/download/#{version}/#{name}-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "b155c7eb57138ec6f79ff524169b8bb27b752adc27f7b27908dfc887a0ec7fd1"
+    url "https://github.com/avencera/#{name}/releases/download/#{version}/#{name}-#{version}-x86_64-apple-darwin.tar.gz"
+    sha256 ""
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/avencera/#{name}/releases/download/#{version}/#{name}-#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "a0b9c074e87a7e7ad7329f6ede20ff45a9570fcdba62f81561b01117afa7f5fb"
+    sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/avencera/#{name}/releases/download/#{version}/#{name}-#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "2caee72784ba61c123538436df3e37106554d7a25854bb5464fa0cb54c6e008e"
+    sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/avencera/#{name}/releases/download/#{version}/#{name}-#{version}-aarch64-unknown-linux-musl.tar.gz"
-    sha256 "77b684cabb5db089cfba4c8c81162b44af2afb3d5f44af7cf91d4dbdaf3de26d"
+    sha256 ""
   end
 
   def install
