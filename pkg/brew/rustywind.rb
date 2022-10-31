eef84feddfedf53c06388a794ee8c58992ff3cb8
Praveen Perera <praveen@avencera.com>
2022-10-31 10:03:02 -0500

    Fix brew scripts

--- pkg/brew/rustywind.rb
+++ pkg/brew/rustywind.rb
@@ -6,7 +6,6 @@
 #   brew uninstall rustywind
 #   brew untap avencera/taps
 class Rustywind < Formula
-  name 'rustywind'
   version 'v0.15.1'
   desc "CLI for organizing Tailwind CSS classes"
   homepage "https://github.com/avencera/rustywind"
@@ -14,26 +13,26 @@ class Rustywind < Formula
 
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/avencera/#{name}/releases/download/#{version}/#{name}-#{version}-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/avencera/rustywind/releases/download/#{version}/rustywind-#{version}-x86_64-apple-darwin.tar.gz"
     sha256 "8e71a5a5a1526c669e3ded23b1917d359c6be53a80ccf5e0b7c656f434e96fb6"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/avencera/#{name}/releases/download/#{version}/#{name}-#{version}-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/avencera/rustywind/releases/download/#{version}/rustywind-#{version}-aarch64-apple-darwin.tar.gz"
     sha256 "af03dc49aa723f144ea5747b49bc9c1ab02239f4344c7ed8cd125d3e386d6bc7"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/avencera/#{name}/releases/download/#{version}/#{name}-#{version}-x86_64-unknown-linux-musl.tar.gz"
+    url "https://github.com/avencera/rustywind/releases/download/#{version}/rustywind-#{version}-x86_64-unknown-linux-musl.tar.gz"
     sha256 "af8703fa778ad0e56a1d9ba22d588d9752d7ed1b9f8b58a9fd40b564f165e5d3"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/avencera/#{name}/releases/download/#{version}/#{name}-#{version}-aarch64-unknown-linux-musl.tar.gz"
+    url "https://github.com/avencera/rustywind/releases/download/#{version}/rustywind-#{version}-aarch64-unknown-linux-musl.tar.gz"
     sha256 "54411952e5e5d058bf5615a4165765216439b9bccd4de192a62d1e8b41b6dc8d"
   end
 
   def install
-     bin.install name
+     bin.install "rustywind"
   end
 end
--- pkg/brew/yamine.rb
+++ pkg/brew/yamine.rb
@@ -6,7 +6,6 @@
 #   brew uninstall yamine
 #   brew untap avencera/taps
 class Yamine < Formula
-  name 'rustywind'
   version 'v0.2.0'
   desc "A simple CLI for combining json and yaml files"
   homepage "https://github.com/avencera/yamine"
@@ -14,26 +13,26 @@ class Yamine < Formula
 
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/avencera/yamine/releases/download/#{version}/#{name}-#{version}-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/avencera/yamine/releases/download/#{version}/yamine-#{version}-x86_64-apple-darwin.tar.gz"
     sha256 "b155c7eb57138ec6f79ff524169b8bb27b752adc27f7b27908dfc887a0ec7fd1"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/avencera/#{name}/releases/download/#{version}/#{name}-#{version}-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/avencera/yamine/releases/download/#{version}/yamine-#{version}-aarch64-apple-darwin.tar.gz"
     sha256 "a0b9c074e87a7e7ad7329f6ede20ff45a9570fcdba62f81561b01117afa7f5fb"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/avencera/#{name}/releases/download/#{version}/#{name}-#{version}-x86_64-unknown-linux-musl.tar.gz"
+    url "https://github.com/avencera/yamine/releases/download/#{version}/yamine-#{version}-x86_64-unknown-linux-musl.tar.gz"
     sha256 "2caee72784ba61c123538436df3e37106554d7a25854bb5464fa0cb54c6e008e"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/avencera/#{name}/releases/download/#{version}/#{name}-#{version}-aarch64-unknown-linux-musl.tar.gz"
+    url "https://github.com/avencera/yamine/releases/download/#{version}/yamine-#{version}-aarch64-unknown-linux-musl.tar.gz"
     sha256 "77b684cabb5db089cfba4c8c81162b44af2afb3d5f44af7cf91d4dbdaf3de26d"
   end
 
   def install
-     bin.install name
+     bin.install "yamine"
   end
 end
